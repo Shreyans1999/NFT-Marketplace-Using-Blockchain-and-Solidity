e6b3322c5f5699cce7bb5f626e04f99f53b40024
Shreyans1999 <saklechashreyans1999@gmail.com>
2023-05-13 11:01:55 +0530

    Added Some npx Commands

--- components/NFTCard/NFTCard.jsx
+++ components/NFTCard/NFTCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { BsImages } from "react-icons/bs";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Link from "next/link";
 
 //INTERNAL IMPORT
